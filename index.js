de890765447ea5d773d90a9c6cfbc16379ce3fc9
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-06-11 11:03:25 +0300

    task-2

--- index.js
+++ index.js
@@ -14,3 +14,18 @@
 // let message = "Не знаете? ECMAScript!";
 // userInput === "ECMAScript" ? (message = "Верно!") : message;
 // alert(message);
+
+//Напишите программу, которая получит от пользователя
+//число (количество минут) и выведет в консоль
+//строку в формате часов и минут
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+// const time = prompt("Введи хвилини");
+// let hours = Math.floor(Number(time) / 60);
+// let minutes = Number(time) % 60;
+
+// hours = String(hours).padStart(2, "0");
+// minutes = String(minutes).padStart(2, "0");
+
+// console.log(`${hours}:${minutes}`);
