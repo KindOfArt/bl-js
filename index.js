26fe7b1ccbdb46c9940ce3ee3b8633ac741c4281
atamaiNia <bimatideka@gmail.com>
2022-06-11 11:21:21 +0300

    task-3

--- index.js
+++ index.js
@@ -29,3 +29,21 @@
 // minutes = String(minutes).padStart(2, "0");
 
 // console.log(`${hours}:${minutes}`);
+
+//3. Напишите цикл, который выводит в консоль
+//числа от max до min по убыванию
+// Додайте все четные числа от min до max
+const max = 50;
+const min = 23;
+let totalSum = 0;
+
+for (let i = max; i >= min; i -= 1) {
+  //   if (i % 2 === 0) {
+  //     totalSum += i;
+  //   }
+  if (i % 2 !== 0) {
+    continue;
+  }
+  totalSum += i;
+}
+console.log(totalSum);
